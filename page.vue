ffab9d0756ef5e7f7c8d8e521c6a0f9fe4f85f17
Mexson Fernandes <robomex2020@gmail.com>
2020-11-22 00:33:58 +0530

    :hammer: page file

---
+++ page.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>Hello SEO</div>
+</template>
+
+<script>
+export default {
+  head() {
+    return this.$seo.getMetaTags('Nice title', 'This is seo helper repo for nuxt.js');
+  },
+}
+</script>
