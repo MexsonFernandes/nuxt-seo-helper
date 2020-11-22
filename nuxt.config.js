b385a2c710d2135eda139543a0b6941c9ed6c6f3
Mexson Fernandes <robomex2020@gmail.com>
2020-11-22 15:13:22 +0530

    :hammer: update config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,44 +1,44 @@
 module.exports = {
-    head: {
-      title: "SEO helper",
-      meta: [
-        {
-          charset: "utf-8"
-        },
-        {
-          name: "viewport",
-          content: "width=device-width, initial-scale=1"
-        },
-        {
-          hid: "description",
-          name: "description",
-          content:
-            "This is seo helper repo for nuxt.js"
-        },
-        {
-          property: "og:description",
-          content:
-            "This is seo helper repo for nuxt.js"
-        },
-      ],
-    },
-    plugins: [
+  head: {
+    title: "SEO helper",
+    meta: [
       {
-        src: "@/plugins/filters.js"
+        charset: "utf-8"
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1"
+      },
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "This is seo helper repo for nuxt.js"
+      },
+      {
+        property: "og:description",
+        content:
+          "This is seo helper repo for nuxt.js"
       },
     ],
-    /*
-     ** Nuxt.js modules
-     */
-    modules: [
-      "@nuxtjs/sitemap",
-      "@nuxtjs/robots",
-    ],
-    sitemap: {
-      hostname: '',
+  },
+  plugins: [
+    {
+      src: "@/plugins/filters.js"
     },
-    robots: {
-      Sitemap: process.env.DOMAIN + "/sitemap.xml",
-    },
+  ],
+  /*
+    ** Nuxt.js modules
+    */
+  modules: [
+    "@nuxtjs/sitemap",
+    "@nuxtjs/robots",
+  ],
+  sitemap: {
+    hostname: '',
+  },
+  robots: {
+    Sitemap: process.env.DOMAIN + "/sitemap.xml",
+  },
 };
   
